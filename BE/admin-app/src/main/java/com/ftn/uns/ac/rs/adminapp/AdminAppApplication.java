ed1ea2672a84e8cb6c0b08a2a2f894ebf601ddd3
Ivana Vlaisavljevic <ivanazvucnik@gmail.com>
2021-03-26 17:32:37 +0100

    init

---
+++ BE/admin-app/src/main/java/com/ftn/uns/ac/rs/adminapp/AdminAppApplication.java
@@ -0,0 +1,13 @@
+package com.ftn.uns.ac.rs.adminapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminAppApplication.class, args);
+	}
+
+}
