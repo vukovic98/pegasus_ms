48b61b4152f79ac177ed3f5e069c3271d2b69347
ivanavlaisavljevic <ivanazvucnik@gmail.com>
2021-04-03 18:25:36 +0200

    CertificateRequest-BE

---
+++ BE/admin-app/src/main/java/com/ftn/uns/ac/rs/adminapp/repository/CertificateRequestRepository.java
@@ -0,0 +1,11 @@
+package com.ftn.uns.ac.rs.adminapp.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ftn.uns.ac.rs.adminapp.beans.CertificateRequest;
+
+@Repository
+public interface CertificateRequestRepository extends JpaRepository<CertificateRequest, Long> {
+
+}
