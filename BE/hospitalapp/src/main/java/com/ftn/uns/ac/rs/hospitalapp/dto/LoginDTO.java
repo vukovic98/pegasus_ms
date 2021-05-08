6d7fc09e80f4d0b53212279a31406ca770e41213
ivanavlaisavljevic <ivanazvucnik@gmail.com>
2021-05-08 17:24:28 +0200

    Hospital app validation

--- BE/hospitalapp/src/main/java/com/ftn/uns/ac/rs/hospitalapp/dto/LoginDTO.java
+++ BE/hospitalapp/src/main/java/com/ftn/uns/ac/rs/hospitalapp/dto/LoginDTO.java
@@ -1,8 +1,15 @@
 package com.ftn.uns.ac.rs.hospitalapp.dto;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
 public class LoginDTO {
 
+	@Email(message = "Email format is not valid.")
+	@NotBlank(message = "Email can not be empty.")
 	private String email;
+	
+	@NotBlank(message = "Password can not be empty.")
 	private String password;
 
 	public LoginDTO() {
