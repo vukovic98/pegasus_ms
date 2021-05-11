3b662cdc45280d8dc21327a49856db38b323371d
ivanavlaisavljevic <ivanazvucnik@gmail.com>
2021-05-11 10:46:37 +0200

    guards for hospital app

--- FE/hospital-app/src/app/app-routing.module.ts
+++ FE/hospital-app/src/app/app-routing.module.ts
@@ -5,14 +5,22 @@ import {LoginComponent} from './components/login/login.component';
 import {CertificateComponent} from './components/certificate/certificate.component';
 import {PatientsComponent} from './components/patients/patients/patients.component';
 import {PatientDetailsComponent} from "./components/patients/patientDetails/patient-details/patient-details.component";
+import {RoleGuard} from "./guards/role.guard";
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'admin-panel', component: AdminPanelComponent },
   { path: 'certificate', component: CertificateComponent },
-  {path: 'patient', component: PatientsComponent},
-  {path: 'patient-details',component:PatientDetailsComponent}
+  { path: 'patient',
+    component: PatientsComponent,
+    canActivate:[RoleGuard],
+    data: {acceptRoles:'ROLE_DOCTOR'}
+  },
+  { path: 'patient-details',
+    component:PatientDetailsComponent,
+    canActivate:[RoleGuard],
+    data: {acceptRoles:'ROLE_DOCTOR'}}
 ];
 
 @NgModule({
