19d3a4399d85faa56d14de7480391e7d3a1c2096
ivanavlaisavljevic <ivanazvucnik@gmail.com>
2021-05-17 13:34:10 +0200

    heart monitor device added

---
+++ BE/devices/heartmonitor/src/main/java/com/ftn/uns/ac/rs/heartmonitor/HeartmonitorApplication.java
@@ -0,0 +1,13 @@
+package com.ftn.uns.ac.rs.heartmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeartmonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeartmonitorApplication.class, args);
+	}
+
+}
