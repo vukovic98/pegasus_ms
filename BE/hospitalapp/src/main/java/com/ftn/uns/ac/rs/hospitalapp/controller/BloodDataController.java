6270fb30355e4c5def66a4346451825b0d7382b2
ivanavlaisavljevic <ivanazvucnik@gmail.com>
2021-06-06 18:04:49 +0200

    templates

--- BE/hospitalapp/src/main/java/com/ftn/uns/ac/rs/hospitalapp/controller/BloodDataController.java
+++ BE/hospitalapp/src/main/java/com/ftn/uns/ac/rs/hospitalapp/controller/BloodDataController.java
@@ -1,5 +1,7 @@
 package com.ftn.uns.ac.rs.hospitalapp.controller;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -9,9 +11,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ftn.uns.ac.rs.hospitalapp.dto.DataRangeDTO;
 import com.ftn.uns.ac.rs.hospitalapp.mongo.proxy.LoggerProxy;
 import com.ftn.uns.ac.rs.hospitalapp.service.BloodDataService;
 import com.ftn.uns.ac.rs.hospitalapp.util.BloodData;
@@ -59,4 +63,15 @@ public class BloodDataController {
 		return new ResponseEntity<>(pageImpl, HttpStatus.OK);
 	}
 
+	@GetMapping(path = "/create-alarm-for-crp")
+	public ResponseEntity<ArrayList<DataRangeDTO>> alarmForCrp(@RequestBody DataRangeDTO dto) {
+
+		boolean ok = true;
+
+		if (ok) {
+			return new ResponseEntity<>(HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
+	}
 }
