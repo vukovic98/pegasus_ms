f2eee610068285b97ab8ebb065ab37e5cd313fbb
Vladimir Vukovic <vladimirvukovic98@hotmail.rs>
2021-06-06 17:57:30 +0200

    blood templates

--- BE/hospitalapp/src/main/java/com/ftn/uns/ac/rs/hospitalapp/dto/DataRangeDTO.java
+++ BE/hospitalapp/src/main/java/com/ftn/uns/ac/rs/hospitalapp/dto/DataRangeDTO.java
@@ -2,6 +2,7 @@ package com.ftn.uns.ac.rs.hospitalapp.dto;
 
 public class DataRangeDTO {
 
+	public long patientID;
 	public double minValue;
 	public double maxValue;
 
@@ -9,10 +10,19 @@ public class DataRangeDTO {
 		super();
 	}
 
-	public DataRangeDTO(double minValue, double maxValue) {
+	public DataRangeDTO(long patientID, double minValue, double maxValue) {
 		super();
+		this.patientID = patientID;
 		this.minValue = minValue;
 		this.maxValue = maxValue;
+	}
+
+	public long getPatientID() {
+		return patientID;
+	}
+
+	public void setPatientID(long patientID) {
+		this.patientID = patientID;
 	}
 
 	public double getMinValue() {
--- BE/hospitalapp/src/main/java/com/ftn/uns/ac/rs/hospitalapp/service/BloodDataService.java
+++ BE/hospitalapp/src/main/java/com/ftn/uns/ac/rs/hospitalapp/service/BloodDataService.java
@@ -1,12 +1,25 @@
 package com.ftn.uns.ac.rs.hospitalapp.service;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 
+import org.apache.maven.shared.invoker.DefaultInvocationRequest;
+import org.apache.maven.shared.invoker.DefaultInvoker;
+import org.apache.maven.shared.invoker.InvocationRequest;
+import org.apache.maven.shared.invoker.Invoker;
+import org.drools.template.ObjectDataCompiler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import com.ftn.uns.ac.rs.hospitalapp.dto.DataRangeDTO;
 import com.ftn.uns.ac.rs.hospitalapp.repository.BloodDataRepository;
 import com.ftn.uns.ac.rs.hospitalapp.util.BloodData;
 
@@ -27,6 +40,134 @@ public class BloodDataService {
 	
 	public Page<BloodData> findAll(Pageable pageable) {
 		return this.bloodDataRepository.findAllByDate(pageable);
+	}
+	
+	public boolean createRuleForCRP(DataRangeDTO dataRange) {
+		try {
+			InputStream template = new FileInputStream(
+					"..\\devices-kjar\\src\\main\\resources\\sbnz\\integracija\\templates\\blood-data-CRP.drt");
+
+			// Compile template to generate new rules
+			List<DataRangeDTO> arguments = new ArrayList<>();
+			arguments.add(dataRange);
+			ObjectDataCompiler compiler = new ObjectDataCompiler();
+			String drl = compiler.compile(arguments, template);
+
+			// Save rule to drl file
+			FileOutputStream drlFile = new FileOutputStream(new File(
+					"..\\devices-kjar\\src\\main\\resources\\sbnz\\integracija\\blood-data-CRP-" + dataRange.patientID + ".drl"));
+			drlFile.write(drl.getBytes());
+			drlFile.close();
+
+			// Update Rules project
+			InvocationRequest request = new DefaultInvocationRequest();
+			request.setPomFile(new File("../devices-kjar/pom.xml"));
+			request.setGoals(Arrays.asList("clean", "install"));
+
+			Invoker invoker = new DefaultInvoker();
+			invoker.setMavenHome(new File(System.getenv("M2_HOME")));
+			invoker.execute(request);
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+	public boolean createRuleForErythrocites(DataRangeDTO dataRange) {
+		try {
+			InputStream template = new FileInputStream(
+					"..\\devices-kjar\\src\\main\\resources\\sbnz\\integracija\\templates\\blood-data-erythrocytes.drt");
+
+			// Compile template to generate new rules
+			List<DataRangeDTO> arguments = new ArrayList<>();
+			arguments.add(dataRange);
+			ObjectDataCompiler compiler = new ObjectDataCompiler();
+			String drl = compiler.compile(arguments, template);
+
+			// Save rule to drl file
+			FileOutputStream drlFile = new FileOutputStream(new File(
+					"..\\devices-kjar\\src\\main\\resources\\sbnz\\integracija\\blood-data-erythrocytes-" + dataRange.patientID + ".drl"));
+			drlFile.write(drl.getBytes());
+			drlFile.close();
+
+			// Update Rules project
+			InvocationRequest request = new DefaultInvocationRequest();
+			request.setPomFile(new File("../devices-kjar/pom.xml"));
+			request.setGoals(Arrays.asList("clean", "install"));
+
+			Invoker invoker = new DefaultInvoker();
+			invoker.setMavenHome(new File(System.getenv("M2_HOME")));
+			invoker.execute(request);
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+	
+	public boolean createRuleForHemoglobin(DataRangeDTO dataRange) {
+		try {
+			InputStream template = new FileInputStream(
+					"..\\devices-kjar\\src\\main\\resources\\sbnz\\integracija\\templates\\blood-data-hemoglobin.drt");
+
+			// Compile template to generate new rules
+			List<DataRangeDTO> arguments = new ArrayList<>();
+			arguments.add(dataRange);
+			ObjectDataCompiler compiler = new ObjectDataCompiler();
+			String drl = compiler.compile(arguments, template);
+
+			// Save rule to drl file
+			FileOutputStream drlFile = new FileOutputStream(new File(
+					"..\\devices-kjar\\src\\main\\resources\\sbnz\\integracija\\blood-data-hemoglobin-" + dataRange.patientID + ".drl"));
+			drlFile.write(drl.getBytes());
+			drlFile.close();
+
+			// Update Rules project
+			InvocationRequest request = new DefaultInvocationRequest();
+			request.setPomFile(new File("../devices-kjar/pom.xml"));
+			request.setGoals(Arrays.asList("clean", "install"));
+
+			Invoker invoker = new DefaultInvoker();
+			invoker.setMavenHome(new File(System.getenv("M2_HOME")));
+			invoker.execute(request);
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+	
+	public boolean createRuleForLeukocytes(DataRangeDTO dataRange) {
+		try {
+			InputStream template = new FileInputStream(
+					"..\\devices-kjar\\src\\main\\resources\\sbnz\\integracija\\templates\\blood-data-leukocytes.drt");
+
+			// Compile template to generate new rules
+			List<DataRangeDTO> arguments = new ArrayList<>();
+			arguments.add(dataRange);
+			ObjectDataCompiler compiler = new ObjectDataCompiler();
+			String drl = compiler.compile(arguments, template);
+
+			// Save rule to drl file
+			FileOutputStream drlFile = new FileOutputStream(new File(
+					"..\\devices-kjar\\src\\main\\resources\\sbnz\\integracija\\blood-data-leukocytes-" + dataRange.patientID + ".drl"));
+			drlFile.write(drl.getBytes());
+			drlFile.close();
+
+			// Update Rules project
+			InvocationRequest request = new DefaultInvocationRequest();
+			request.setPomFile(new File("../devices-kjar/pom.xml"));
+			request.setGoals(Arrays.asList("clean", "install"));
+
+			Invoker invoker = new DefaultInvoker();
+			invoker.setMavenHome(new File(System.getenv("M2_HOME")));
+			invoker.execute(request);
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
 	}
 	
 }
